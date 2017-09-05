40568ba11ba459399ab9ee9f1337062a83bf3c35
Karl Pokus <karl.pokus@gmail.com>
2017-09-05 18:02:16 +0200

    refactoring

--- simple/consumer.js
+++ simple/consumer.js
@@ -1,16 +1,17 @@
 const amqp = require('amqplib');
 const q = 'tasks';
 
+function logger(msg) {
+  const ch = this;
+  if (msg) {
+    console.log(msg.content.toString());
+    ch.ack(msg);
+  }
+}
+
 amqp.connect('amqp://localhost')
   .then(conn => conn.createChannel())
   .then(ch => ch.assertQueue(q)
-    .then(ok => {
-      return ch.consume(q, msg => {
-        if (msg) {
-          console.log(msg.content.toString());
-          ch.ack(msg);
-        }
-      });
-    })
+    .then(ok => ch.consume(q, logger.bind(ch)))
   )
   .catch(console.warn);
--- simple/publisher.js
+++ simple/publisher.js
@@ -1,9 +1,14 @@
 const amqp = require('amqplib');
 const q = 'tasks';
+let i = 0;
+
+function publishTasks(ch) {
+  ch.sendToQueue(q, Buffer.from(`task ${++i}`));
+}
 
 amqp.connect('amqp://localhost')
   .then(conn => conn.createChannel())
   .then(ch => ch.assertQueue(q)
-    .then(ok => ch.sendToQueue(q, Buffer.from('task A')))
+    .then(ok => setInterval(publishTasks.bind(null, ch), 5000))
   )
   .catch(console.warn);
